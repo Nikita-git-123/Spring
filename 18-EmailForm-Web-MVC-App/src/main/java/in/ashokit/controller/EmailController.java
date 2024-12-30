4214c6ee5852a184382022f073ab6fbd9306bd6e
Nikita <patilniku.2645@gmail.com>
2024-12-30 14:38:37 +0530

    updates

--- 18-EmailForm-Web-MVC-App/src/main/java/in/ashokit/controller/EmailController.java
+++ 18-EmailForm-Web-MVC-App/src/main/java/in/ashokit/controller/EmailController.java
@@ -1,37 +1,29 @@
 package in.ashokit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import in.ashokit.service.EmailService;
-import jakarta.mail.MessagingException;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/send-email")
+@ResponseBody
 public class EmailController {
 
 	@Autowired
-	private EmailService emailService;
+	private JavaMailSender mailSender;
 
-	@GetMapping
-	public String showEmailForm() {
-		return "emailForm"; // The name of your HTML form (emailForm.html)
-	}
-
-	@PostMapping
-	public String sendEmail(@RequestParam("email") String toEmail, Model model) {
-		try {
-			// Sending email to the provided email address
-			emailService.sendEmail(toEmail, "Test Subject", "Hello, this is a test email.");
-			model.addAttribute("message", "Email sent successfully!");
-		} catch (MessagingException e) {
-			model.addAttribute("message", "Failed to send email. Please try again.");
-		}
-		return "emailForm"; // You can redirect or return a new page
+	@GetMapping("/email")
+	public String sendEmail() throws Exception {
+		
+		SimpleMailMessage msg = new SimpleMailMessage();
+		
+		msg.setTo("patilniku.2645@gmail.com");
+		msg.setSubject("Welcome to mail service");
+		
+		mailSender.send(msg);
+		
+		return "Successfull.......";
 	}
 }
